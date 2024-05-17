2540f2bc1ab1e44f5aa9b1c27c511b0ff09627ad
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2024-05-17 11:20:36 +0000

    chore: change time unit to hour
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- src/pymfm/control/algorithms/rule_based.py
+++ src/pymfm/control/algorithms/rule_based.py
@@ -181,7 +181,7 @@ def scheduling(P_load_gen: pd.Series, battery_specs: BatterySpecs, delta_T: time
             "P_net_after_kW",
             "P_bat_kW",
             "SoC_bat",
-            "bat_energy_kWs",
+            "bat_energy_kWh",
             "import_kW",
             "export_kW",
         ],
@@ -189,20 +189,22 @@ def scheduling(P_load_gen: pd.Series, battery_specs: BatterySpecs, delta_T: time
     )
     output_ds.import_kW = 0
     output_ds.export_kW = 0
-    output_ds.bat_energy_kWs = 0
-    bat_initial_Energy_kWs =  battery_specs.initial_SoC * battery_specs.bat_capacity_kWs
+    output_ds.bat_energy_kWh = 0
+    bat_min_Energy_kWh = battery_specs.min_SoC * battery_specs.bat_capacity_kWh
+    bat_max_Energy_kWh = battery_specs.max_SoC * battery_specs.bat_capacity_kWh
+    bat_initial_Energy_kWh =  battery_specs.initial_SoC * battery_specs.bat_capacity_kWh
     # Convert timedelta to float in terms of seconds
-    delta_time_in_sec = delta_T.total_seconds()
+    delta_T_h = delta_T.total_seconds() / 3600 # 3600 seconds per hour
     output_ds.P_bat_kW = P_load_gen.P_load_kW - P_load_gen.P_gen_kW
 
     if output_ds.P_bat_kW > 0:
-        output_ds.bat_energy_kWs = bat_initial_Energy_kWs - (
-            battery_specs.dis_efficiency * output_ds.P_bat_kW * delta_time_in_sec
+        output_ds.bat_energy_kWh = bat_initial_Energy_kWh - (
+            battery_specs.dis_efficiency * output_ds.P_bat_kW * delta_T_h
         )
         output_ds.P_bat_kW = output_ds.P_bat_kW / battery_specs.dis_efficiency
     else:
-        output_ds.bat_energy_kWs = (
-            bat_initial_Energy_kWs - (output_ds.P_bat_kW * delta_time_in_sec) / battery_specs.ch_efficiency
+        output_ds.bat_energy_kWh = (
+            bat_initial_Energy_kWh - (output_ds.P_bat_kW * delta_T_h) / battery_specs.ch_efficiency
         )
         output_ds.P_bat_kW = output_ds.P_bat_kW * battery_specs.ch_efficiency
     # discharging
@@ -211,15 +213,15 @@ def scheduling(P_load_gen: pd.Series, battery_specs: BatterySpecs, delta_T: time
         if abs(output_ds.P_bat_kW) >= battery_specs.P_dis_max_kW:
             output_ds.import_kW = output_ds.P_bat_kW - battery_specs.P_dis_max_kW
             output_ds.P_bat_kW = battery_specs.P_dis_max_kW
-            output_ds.bat_energy_kWs = bat_initial_Energy_kWs - (
-                battery_specs.dis_efficiency * battery_specs.P_dis_max_kW * delta_time_in_sec
+            output_ds.bat_energy_kWh = bat_initial_Energy_kWh - (
+                battery_specs.dis_efficiency * battery_specs.P_dis_max_kW * delta_T_h
             )
-        if output_ds.bat_energy_kWs < battery_specs.min_SoC * battery_specs.bat_capacity_kWs:
+        if output_ds.bat_energy_kWh < bat_min_Energy_kWh:
             output_ds.import_kW = output_ds.import_kW + (
-                (battery_specs.min_SoC * battery_specs.bat_capacity_kWs - output_ds.bat_energy_kWs) / delta_time_in_sec
+                (bat_min_Energy_kWh - output_ds.bat_energy_kWh) / delta_T_h
             )
             output_ds.P_bat_kW = act_ptcb - output_ds.import_kW
-            output_ds.bat_energy_kWs = battery_specs.min_SoC * battery_specs.bat_capacity_kWs
+            output_ds.bat_energy_kWh = bat_min_Energy_kWh
     # charging
     if output_ds.P_bat_kW < 0:
         act_ptcb = output_ds.P_bat_kW
@@ -228,19 +230,19 @@ def scheduling(P_load_gen: pd.Series, battery_specs: BatterySpecs, delta_T: time
         else:
             output_ds.export_kW = abs(output_ds.P_bat_kW) - battery_specs.P_ch_max_kW
             output_ds.P_bat_kW = -battery_specs.P_ch_max_kW
-            output_ds.bat_energy_kWs = (
-                bat_initial_Energy_kWs + (battery_specs.P_ch_max_kW * delta_time_in_sec) / battery_specs.ch_efficiency
+            output_ds.bat_energy_kWh = (
+                bat_initial_Energy_kWh + (battery_specs.P_ch_max_kW * delta_T_h) / battery_specs.ch_efficiency
             )
-        if output_ds.bat_energy_kWs > battery_specs.max_SoC * battery_specs.bat_capacity_kWs:
+        if output_ds.bat_energy_kWh > bat_max_Energy_kWh:
             output_ds.export_kW = output_ds.export_kW + (
-                (output_ds.bat_energy_kWs - battery_specs.max_SoC * battery_specs.bat_capacity_kWs) / delta_time_in_sec
+                (output_ds.bat_energy_kWh - bat_max_Energy_kWh) / delta_T_h
             )
             output_ds.P_bat_kW = -(abs(act_ptcb) - (output_ds.export_kW))
-            output_ds.bat_energy_kWs = battery_specs.max_SoC * battery_specs.bat_capacity_kWs
+            output_ds.bat_energy_kWh = bat_max_Energy_kWh
         output_ds.P_bat_kW = float(output_ds.P_bat_kW)
     output_ds.P_net_before_kW = P_load_gen.P_load_kW - P_load_gen.P_gen_kW
     output_ds.P_net_after_kW = -output_ds.export_kW + output_ds.import_kW
-    output_ds.SoC_bat = (output_ds.bat_energy_kWs / battery_specs.bat_capacity_kWs) * 100
+    output_ds.SoC_bat = (output_ds.bat_energy_kWh / battery_specs.bat_capacity_kWh) * 100
     output_ds.P_bat_kW = output_ds.P_bat_kW * -1  # charging: positiv, discharging: negativ
 
     return output_ds
