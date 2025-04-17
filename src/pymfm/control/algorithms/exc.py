e2a3e080168138ed27c34e69d73a41f303252cda
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2025-04-17 11:48:09 +0000

    feat: better handling for infeasable
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

---
+++ src/pymfm/control/algorithms/exc.py
@@ -0,0 +1,2 @@
+class InfeasableError(RuntimeError):
+    pass
--- src/pymfm/control/algorithms/optimization_based.py
+++ src/pymfm/control/algorithms/optimization_based.py
@@ -26,9 +26,9 @@ import os
 from typing import List
 
 import pandas as pd
+from pymfm.control.algorithms.exc import InfeasableError
 import pyomo.kernel as pmo
-from pyomo.core import (ConcreteModel, Constraint, NonNegativeReals, Objective,
-                        Var, minimize)
+from pyomo.core import ConcreteModel, Constraint, NonNegativeReals, Objective, Var, minimize
 from pyomo.environ import SolverFactory
 from pyomo.opt import SolverStatus, TerminationCondition
 
@@ -569,10 +569,13 @@ def scheduling(
     model.obj = Objective(rule=obj_rule, sense=minimize)
     solver = optimization_solver.solve(model).solver
 
-
     from pyomo.util.infeasible import log_infeasible_constraints
+
     print("--------------------------------------")
-    print(log_infeasible_constraints(model))
+    log_infeasible_constraints(model)
+    if solver.termination_condition in (TerminationCondition.infeasible, TerminationCondition.infeasibleOrUnbounded):
+        raise InfeasableError("Problem was not solvable")
+
     #####################################################################################################
     ##################################       POST PROCESSING             ################################
     # Initialize DataFrames and Series to store post-processing results
